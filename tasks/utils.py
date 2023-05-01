c02cedf692d3b7e55eba13f12535ca3b6aacbcf1
Maria Afara <47759383+mariaafara@users.noreply.github.com>
2023-05-01 16:13:26 +0200

    Update airflow version + add minio + smtp provider + minor project structure change (#1)
    * update airflow version
    
    * Update dags path
    
    * Add minio
    
    * setup minio
    
    * add smtop provider requirment

--- tasks/utils.py
+++ tasks/utils.py
@@ -7,7 +7,7 @@ def get_project_dir(ctx) -> Path:
 
 def docker_compose_command(ctx, command, version=2):
     docker_compose_folder_path = get_project_dir(ctx).joinpath("docker-compose")
-    compose_files = ["airflow"]
+    compose_files = ["airflow", "minio"]
     compose_cmd = "docker compose" if version == 2 else "docker-compose"
     cmd = f"{compose_cmd} {' '.join([f'-f {docker_compose_folder_path.joinpath(file)}.yml' for file in compose_files])} {command}"
     print(cmd)
